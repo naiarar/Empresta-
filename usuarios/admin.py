870a76e6872c11e3d4353fb277a4394543b9b5c7
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-09 19:20:25 -0300

    campos registro de usuario somente leitura

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from usuarios.models import Usuario
 
-admin.site.register(Usuario)
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('nome', 'email', 'senha')
