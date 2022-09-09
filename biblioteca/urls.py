010c900936603ce5d9f0b91141048c04bf7743d6
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-09 18:44:12 -0300

    alteracoes realizadas

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+import usuarios
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('livro/', include('livro.urls'))
-    path('auth/', include(usuarios.urls))
+    path('livro/', include('livro.urls')),
+    path('auth/', include('usuarios.urls')),
+    
+
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('login/', views.login, name = 'login')
+    path ('login/', views.login, name = 'login'),
     path('cadastro/', views.cadastro, name ='cadastro')
 
 
