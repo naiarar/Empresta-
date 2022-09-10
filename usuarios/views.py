ba91e7a9e3cca5921320d375866ba12d3edd9db7
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-10 19:28:07 -0300

    criando hashlib para senha não ser mostrada no bd

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Usuario
 from django.shortcuts import redirect
+from hashlib import sha256
 
 def login(request):
     return HttpResponse('login') 
@@ -17,15 +18,19 @@ def valida_cadastro(request):
     usuario=Usuario.objects.filter(email = email)
 
     if len(nome.strip()) == 0 or len (email.strip()) == 0:
-        return redirect('/auth/login/?status=1')
+        return redirect('/auth/cadastro/?status=1')
+
+    if len(senha)<4:
+        return redirect('/auth/cadastro/?status=2')
 
     if len(usuario) > 0:  
-        return redirect('/auth/login/?status=2')
+        return redirect('/auth/cadastro/?status=3')
 
     try:
+        senha=sha256(senha.encode()).hexdigest()
         usuario = Usuario(nome = nome, senha = senha, email = email)
         usuario.save()
-        return redirect('/auth/login/?status=0')
+        return redirect('/auth/cadastro/?status=0')
     except:
-        return redirect('/auth/login/?status=3')
+        return redirect('/auth/cadastro/?status=4')
 
