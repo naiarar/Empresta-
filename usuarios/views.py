c9baec6e125a31a93d0314086e6b962f22a85308
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-12 12:21:42 -0300

    fazendo get do status

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import redirect
 from hashlib import sha256
 
 def login(request):
-    return render(request, 'login.html') 
+    status = request.GET.get('status')
+    return render(request, 'login.html', {'status': status}) 
 
 def cadastro(request):
     status = request.GET.get('status')
@@ -38,6 +39,17 @@ def valida_cadastro(request):
 def valida_login(request):
     email = request.POST.get('email')
     senha = request.POST.get('senha')
+
+    senha = sha256(senha.encode()).hexdigest()
+
+    usuario = Usuario.objects.filter(email = email).filter(senha = senha)
+
+    if len(usuario) == 0:
+        return redirect ('/auth/login/?status=1')
+    elif len(usuario) > 0:
+        request.session['usuario'] = usuario[0].id
+        return redirect('/livro/home/')
+
     return HttpResponse(f"{email} {senha}")
 
 
