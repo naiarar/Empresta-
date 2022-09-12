56485393a9c881bc83dfb1fbcb04e44bf05d7f25
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-12 15:08:11 -0300

    def funcao buscar e exibir nome usuario

--- livro/views.py
+++ livro/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from usuarios.models import Usuario
+
 
 def home(request):
-    return HttpResponse('Minha home')
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        return HttpResponse(f'Bem vindo {usuario}')
+    else:
+        return redirect('/auth/login/?status=2')
