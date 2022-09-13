ba59980aa5a971fb1ddb814a6a2fd65f467b373c
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-13 10:18:38 -0300

    Inseri categoria  eusuario puxando a fore

--- livro/models.py
+++ livro/models.py
@@ -2,16 +2,19 @@ from email import generator
 from django.db import models
 from uuid import uuid4
 from datetime import datetime
+from usuarios.models import Usuario
 
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
     descricao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
 class Livros(models.Model):
     id_livro = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     titulo = models.CharField(max_length=100)
     autor = models.CharField(max_length=50)
-    genero = models.CharField(max_length=50)
     qnt_pag = models.IntegerField()
     editora = models.CharField(max_length=50)
     ano_publi = models.DateField(max_length=4)
@@ -21,6 +24,9 @@ class Livros(models.Model):
     nome_emprestado = models.CharField(max_length=50, blank=True, null=True)
     data_emprestimo = models.DateTimeField(blank=True, null=True)
     data_devolucao = models.DateTimeField(blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+
 
  
 
