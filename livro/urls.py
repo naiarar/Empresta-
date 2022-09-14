374ae789a4f74cc351aa091c47f00cb018c8f004
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-14 18:31:30 -0300

    adc url historico de emprestimo

--- livro/urls.py
+++ livro/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('ver_livro/<int:id>', views.ver_livros, name = 'ver_livros' ),
+    path('historico_emprestimos/<int:id>', views.historico_emprestimos, name = 'historico_emprestimos' ),
 
 ]
