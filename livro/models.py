71490de2245d858d9c0b794565502407bc3bd6a0
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-14 18:31:47 -0300

    criei class emprestimo

--- livro/models.py
+++ livro/models.py
@@ -12,12 +12,6 @@ class Categoria(models.Model):
     def __str__(self) -> str:
         return self.nome
 
-class Emprestimo(models.Model):
-    nome_emprestado = models.CharField(max_length=50, blank=True, null=True)
-    data_emprestimo = models.DateTimeField(blank=True, null=True)
-    data_devolucao = models.DateTimeField(blank=True, null=True)
-    categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
-    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 class Livros(models.Model):
     titulo = models.CharField(max_length=100)
     autor = models.CharField(max_length=50)
@@ -27,10 +21,8 @@ class Livros(models.Model):
     nota = models.IntegerField()
     data_cadastro = models.DateTimeField(auto_now_add=True)
     emprestado = models.BooleanField(default=False)
-
-
-
- 
+    categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = 'Livro'
@@ -38,6 +30,16 @@ class Livros(models.Model):
     def __str__(self):
         return self.titulo
 
+
+ 
+class Emprestimo(models.Model):
+    nome_emprestado = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    data_emprestimo = models.DateField(blank=True, null=True)
+    data_devolucao = models.DateField(blank=True, null=True)
+    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.nome_emprestado} | {self.livro}'
 
 
 
