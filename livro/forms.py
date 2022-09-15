be3ff4300cacf64a246524c83cd112a27d328679
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-15 14:32:33 -0300

    criei forms que herdou de Livros

---
+++ livro/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.db.models import fields
+from .models import Livros
+
+
+class CadastroLivro(forms.ModelForm):
+    class Meta: 
+        model = Livros
+        fields = ('titulo', 'autor', 'qnt_pag', 'editora', 'ano_publi', 'nota', 'categoria', 'usuario')
+
+
