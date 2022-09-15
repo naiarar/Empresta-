dec731f62ea2a4df6aba57bb9e62beb1a174e315
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-15 14:31:51 -0300

    url cadastrar_livro

--- livro/urls.py
+++ livro/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('ver_livro/<int:id>', views.ver_livros, name = 'ver_livros' ),
     path('historico_emprestimos/<int:id>', views.historico_emprestimos, name = 'historico_emprestimos' ),
+    path('cadastrar_livro', views.cadastrar_livro, name = 'cadastrar_livro'),
+
 
 ]
