f0d56bc59f155ebd7bd9adb929696ae8436ff2e9
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-18 19:33:23 -0300

    alterei widget de usuario, de selecta para hidden

--- livro/forms.py
+++ livro/forms.py
@@ -10,6 +10,7 @@ class CadastroLivro(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['usuario'].widget = forms.HiddenInput
 
 
 
