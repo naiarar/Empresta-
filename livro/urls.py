9f0bee8addd3cce31d15e9b3863816e4db8263f4
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-20 10:50:20 -0300

    adc url excluir livro

--- livro/urls.py
+++ livro/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('ver_livro/<int:id>', views.ver_livros, name = 'ver_livros' ),
     path('historico_emprestimos/<int:id>', views.historico_emprestimos, name = 'historico_emprestimos' ),
     path('cadastrar_livro', views.cadastrar_livro, name = 'cadastrar_livro'),
+    path('excluir_livro/<int:id>', views.excluir_livro, name = 'excluir_livro'),
 
 
 ]
