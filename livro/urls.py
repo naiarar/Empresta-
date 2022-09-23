dc7122ad1645f4d21ef9758018b37e57de2063ea
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-23 18:24:29 -0300

    adc url seus emprwetimos

--- livro/urls.py
+++ livro/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('excluir_livro/<int:id>', views.excluir_livro, name = 'excluir_livro'),
     path('emprestar_livro/<int:id>', views.emprestar_livro, name = 'emprestar_livro'),
     path('devolver_livro/<int:id>', views.devolver_livro, name = 'devolver_livro'),
+    path('seus_emprestimos', views.seus_emprestimos, name = 'seus_emprestimos'),
 
 
 ]
