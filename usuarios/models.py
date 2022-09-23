1591ecbd25d7a77c7b240cfbef29561e271fc924
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-23 18:25:41 -0300

    modelei cadastro completo

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from datetime import date
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
-    senha = models.CharField(max_length=64)
+    data_nas = models.DateField(null=True, blank=False)
+    cep = models.BigIntegerField(null=True, blank=False)
+    rua = models.CharField(max_length=70, null=True, blank=False)
+    numero = models.CharField(max_length=5, null=True, blank=False)
+    complemento = models.CharField(max_length=15, null=True, blank=False)
+    bairro = models.CharField(max_length=15, null=True, blank=False)
+    cidade = models.CharField(max_length=15, null=True, blank=False)
+    whatsapp = models.CharField(max_length=30, null=True, blank=False)
+    instagram = models.CharField(max_length=30, null=True, blank=False)
+    senha = models.CharField(max_length=10)
 
     def __str__(self) -> str:
         return self.nome
