ab2c1d8ca97166d3ba781fae8cb718d59266cbc3
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-26 09:53:52 -0300

    inclui url imagem com imports novos

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 import usuarios
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('auth/', include('usuarios.urls')),
     
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
