bab4f51b1e766d46350260cdb0710eb7ac946045
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-29 19:24:22 -0300

    correcao identação

--- livro/urls.py
+++ livro/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
-    path('ver_livro/<int:id>', views.ver_livros, name = 'ver_livros' ),
-    path('historico_emprestimos/<int:id>', views.historico_emprestimos, name = 'historico_emprestimos' ),
-    path('cadastrar_livro', views.cadastrar_livro, name = 'cadastrar_livro'),
-    path('excluir_livro/<int:id>', views.excluir_livro, name = 'excluir_livro'),
-    path('emprestar_livro/<int:id>', views.emprestar_livro, name = 'emprestar_livro'),
-    path('devolver_livro/<int:id>', views.devolver_livro, name = 'devolver_livro'),
-    path('seus_emprestimos', views.seus_emprestimos, name = 'seus_emprestimos'),
-    path('processa_avaliacao', views.processa_avaliacao, name = 'processa_avaliacao'),
+    path('home/', views.home, name='home'),
+    path('ver_livro/<int:id>', views.ver_livros, name='ver_livros'),
+    path('historico_emprestimos/<int:id>',
+         views.historico_emprestimos, name='historico_emprestimos'),
+    path('cadastrar_livro', views.cadastrar_livro, name='cadastrar_livro'),
+    path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
+    path('emprestar_livro/<int:id>', views.emprestar_livro, name='emprestar_livro'),
+    path('devolver_livro/<int:id>', views.devolver_livro, name='devolver_livro'),
+    path('seus_emprestimos', views.seus_emprestimos, name='seus_emprestimos'),
+    path('processa_avaliacao', views.processa_avaliacao, name='processa_avaliacao'),
 
 
 ]
