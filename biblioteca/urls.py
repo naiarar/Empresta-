7692f4ebda48e2e7b9001cc2c1e9e85e436abbaf
Naiara Rodrigues <rnaiara92@gmail.com>
2022-09-29 19:25:23 -0300

    url index

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+
+from index.views import index
 
 import usuarios
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
     path('auth/', include('usuarios.urls')),
-    
 
-] 
+
+
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.CSS_URL, document_root=settings.CSS_ROOT)
