92c4d3fe30ba703394d53346d0e6efac0a917fba
Naiara Rodrigues <rnaiara92@gmail.com>
2023-02-09 16:25:08 -0300

    diminui daDOS NECESSARIOS

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,18 +3,13 @@ from datetime import date
 
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=30)
+    # nome = models.CharField(max_length=30)
+    # email = models.EmailField(null=True, blank=True)
+    # senha = models.CharField(max_length=10, null=True, blank=True)
+
+    nome = models.CharField(max_length=10)
     email = models.EmailField()
-    data_nas = models.DateField(null=True, blank=False)
-    cep = models.BigIntegerField(null=True, blank=False)
-    rua = models.CharField(max_length=70, null=True, blank=False)
-    numero = models.CharField(max_length=5, null=True, blank=False)
-    complemento = models.CharField(max_length=15, null=True, blank=False)
-    bairro = models.CharField(max_length=15, null=True, blank=False)
-    cidade = models.CharField(max_length=15, null=True, blank=False)
-    whatsapp = models.CharField(max_length=30, null=True, blank=False)
-    instagram = models.CharField(max_length=30, null=True, blank=False)
-    senha = models.CharField(max_length=10)
+    senha = models.CharField(max_length=5)
 
     def __str__(self) -> str:
         return self.nome
