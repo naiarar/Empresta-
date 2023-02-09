7908a2b945d402ca98eaf90985fd3681a215d76d
Naiara Rodrigues <rnaiara92@gmail.com>
2023-02-09 15:38:05 -0300

    funcionando

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -13,7 +13,12 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # take environment variables from .env.
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
